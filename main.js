17a21bf43a10cb5c035db65f293a8a8c66770c2e
ang-sant-abril <ang-sant-abril@gmail.com>
2021-04-12 08:43:34 +0000

    ends

--- main.js
+++ main.js
@@ -2,23 +2,23 @@
 // Sintaxis de declaración
 // let variable: tipo;
 // string
-let mensaje;
+var mensaje;
 mensaje = 'Hola Mundo!';
 // mensaje = 12; Error
 // number
-let resultado;
+var resultado;
 resultado = 9.5;
 // boolean
-let mayorEdad;
+var mayorEdad;
 mayorEdad = true;
 // Tipos complejos
 // Arrays
 // let identificador: tipo-elemento[] ó bien let identificador: Array<tipo-elemento>
-let frutas;
-let vehiculos;
+var frutas;
+var vehiculos;
 frutas = ['peras', 'manzanas'];
 // Tipo any (romper la inferencia de tipos)
-let id = 2876;
+var id = 2876;
 id = '06754';
 // Tipado de funciones (parámetros y salida)
 function suma(a, b) {
@@ -31,5 +31,58 @@ function setMensaje(mensaje) {
 function devResultado(a) {
     return a;
 }
-const b = devResultado('Hola');
-const c = devResultado(1234);
+var b = devResultado('Hola');
+var c = devResultado(1234);
+var referencia = 12;
+referencia = '02345';
+var razaToby = 'Pastor Alemán';
+// Clases 
+var Jugador = /** @class */ (function () {
+    function Jugador(nombre, apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+    Jugador.prototype.setGoles = function (goles) {
+        this.goles = goles;
+    };
+    Jugador.prototype.getGoles = function () {
+        return this.goles;
+    };
+    return Jugador;
+}());
+var jugador1 = new Jugador('Lionel', 'Messi');
+var jugadores;
+jugadores = [
+    jugador1,
+    // {nombre: 'Cristiano', apellidos: 'Ronaldo'} error
+];
+// Constructor breve (Sintaxis para clases)
+var Player = /** @class */ (function () {
+    function Player(name, surname, goals) {
+        this.name = name;
+        this.surname = surname;
+        this.goals = goals;
+    }
+    Player.prototype.setGoals = function (goals) {
+        this.goals = goals;
+    };
+    Player.prototype.getGoals = function () {
+        return this.goals;
+    };
+    return Player;
+}());
+var sendButtonState = {
+    hidden: false,
+    disabled: true,
+    waiting: false
+};
+var Proveedor = /** @class */ (function () {
+    function Proveedor(razonSocial, cif) {
+        this.razonSocial = razonSocial;
+        this.cif = cif;
+    }
+    Proveedor.prototype.getCif = function () {
+        return this.cif;
+    };
+    return Proveedor;
+}());
--- main.ts
+++ main.ts
@@ -56,3 +56,104 @@ function devResultado<T> (a: T): T {
 const b = devResultado<string> ('Hola');
 const c = devResultado<number> (1234);
 
+// Tipos de unión
+
+type _id = string | number;
+
+let referencia: _id = 12;
+referencia = '02345';
+
+// Tipos de unión complejos
+
+type razasPerro = 'Pastor Alemán' | 'Pastor Belga' | 'Mastín';
+
+let razaToby: razasPerro = 'Pastor Alemán';
+
+// Clases 
+
+class Jugador {
+    public nombre: string; // Si no se define el modificador es public por defecto
+    public apellidos: string;
+    private goles: number;
+
+    constructor(nombre: string, apellidos: string){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    setGoles(goles: number): void {
+        this.goles = goles;
+    }
+
+    getGoles(): number {
+        return this.goles;
+    }
+}
+
+let jugador1 = new Jugador('Lionel','Messi');
+
+let jugadores: Array<Jugador>;
+
+jugadores = [
+    jugador1,
+    // {nombre: 'Cristiano', apellidos: 'Ronaldo'} error
+]
+
+// Constructor breve (Sintaxis para clases)
+
+class Player {
+
+    constructor(public name: string, public surname: string, private goals: number) {
+
+    }
+
+    setGoals(goals: number): void {
+        this.goals = goals;
+    }
+
+    getGoals(): number {
+        return this.goals;
+    }
+}
+
+// Interfaces (tipado estructural)
+
+interface ButtonState {
+    hidden: boolean;
+    disabled: boolean;
+    waiting: boolean;
+    backgroundColor?: string;
+}
+
+let sendButtonState: ButtonState = {
+    hidden: false,
+    disabled: true,
+    waiting: false,
+    // backgroundColor: 'white'
+}
+
+// Interfaces y clases (implementación en clases)
+
+interface DatosMaestros {
+    razonSocial: string;
+    cif: string;
+
+    getCif(): string;
+}
+
+class Proveedor implements DatosMaestros {
+    razonSocial: string;
+    cif: string;
+    formaPago: string;
+
+    constructor(razonSocial: string, cif: string) {
+        this.razonSocial = razonSocial;
+        this.cif = cif;
+    }
+
+    getCif(): string {
+        return this.cif;
+    }
+
+    //
+}
